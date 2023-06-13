351f8a6e2e9db42749a8b42098959508a98af772
Balasadi <110322911+Balasadi@users.noreply.github.com>
2023-06-13 16:45:54 -0700

    Update settings.py

--- resume_app/settings.py
+++ resume_app/settings.py
@@ -79,13 +79,8 @@ WSGI_APPLICATION = 'resume_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'profile',
-        'USER': 'postgres',
-        'PASSWORD': 'Abcd1234',
-        'HOST': 'localhost',
-        
-    
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
